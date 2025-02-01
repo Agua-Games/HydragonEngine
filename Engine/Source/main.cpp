831cd3018a94e6e8efa995b40d8c25ea50e406ca
Athos Correia Sampaio <athos.sampaio@gmail.com>
2025-02-01 16:39:55 -0300

    removing the submodules to add again from scratch cleanly
    ...

---
+++ Engine/Source/main.cpp
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2024 Agua Games. All rights reserved.
+ * Licensed under the Agua Games License 1.0
+ *
+ * Hydragon Engine's main entry point.
+ */
+#include <iostream>
+
+int main() {
+    // Hydragon in the egg
+    std::cout << "Hello, Hydragon Engine!" << std::endl;
+    return 0;
+}
