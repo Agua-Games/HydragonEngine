9757ab49ac84a24b1b7d9bcd864c1bf31f39b57e
Athos Correia Sampaio <athos.sampaio@gmail.com>
2025-02-17 23:05:37 -0300

    Right toolbar layout
    ...

--- Engine/Source/Core/UI/RightToolbar.cpp
+++ Engine/Source/Core/UI/RightToolbar.cpp
@@ -2,20 +2,57 @@
  * Copyright (c) 2024 Agua Games. All rights reserved.
  * Licensed under the Agua Games License 1.0
  */
-#pragma once
-#include <imgui.h>  // Include ImGui's header
+#include <imgui.h>
+#include <imgui_internal.h>
 #include <string>
 
 #include "RightToolbar.h"
 #include "hdImgui.h"
+#include "IconsMaterialSymbols.h"
 
 namespace hdImgui {
 void ShowRightToolbar(bool* p_open, HdEditorWindowData* windowData) 
 {
-    if (ImGui::Begin("Right Toolbar", p_open))
+    ImGuiWindowClass window_class;
+    window_class.DockNodeFlagsOverrideSet = ImGuiDockNodeFlags_AutoHideTabBar;
+    ImGui::SetNextWindowClass(&window_class);
+
+    // Style settings for a compact vertical toolbar
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(4, 4));
+    ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(4, 4));
+    ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(4, 2));
+    
+    ImVec2 buttonSize = windowData->iconDefaultSize;
+
+    if (ImGui::Begin("Right Toolbar", p_open, 
+        ImGuiWindowFlags_NoTitleBar | 
+        ImGuiWindowFlags_NoScrollbar | 
+        ImGuiWindowFlags_NoScrollWithMouse))
     {
-        ImGui::Button("Test1");
+        // Scene Management Tools
+        if (ImGui::Button(ICON_MS_LAYERS "##Layers", buttonSize)) { /* Handle Layers Panel */ }
+        if (ImGui::IsItemHovered()) ImGui::SetTooltip("Layers");
+
+        if (ImGui::Button(ICON_MS_SETTINGS "##Properties", buttonSize)) { /* Handle Properties Panel */ }
+        if (ImGui::IsItemHovered()) ImGui::SetTooltip("Properties");
+
+        ImGui::Separator();
+
+        // Asset Management
+        if (ImGui::Button(ICON_MS_FOLDER "##Assets", buttonSize)) { /* Handle Asset Browser */ }
+        if (ImGui::IsItemHovered()) ImGui::SetTooltip("Asset Browser");
+
+        if (ImGui::Button(ICON_MS_PALETTE "##Materials", buttonSize)) { /* Handle Material Editor */ }
+        if (ImGui::IsItemHovered()) ImGui::SetTooltip("Materials");
+
+        ImGui::Separator();
+
+        // Scene Preview
+        if (ImGui::Button(ICON_MS_PREVIEW "##Preview", buttonSize)) { /* Handle Scene Preview */ }
+        if (ImGui::IsItemHovered()) ImGui::SetTooltip("Scene Preview");
     }
     ImGui::End();
+
+    ImGui::PopStyleVar(3);
 }
 } // namespace hdImgui
