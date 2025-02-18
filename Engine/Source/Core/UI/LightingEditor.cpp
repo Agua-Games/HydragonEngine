3615cd43514f4c1b70ce2869ec3a6ae5127aa2cd
Athos Correia Sampaio <athos.sampaio@gmail.com>
2025-02-18 02:24:26 -0300

    Lighting Editor UI, beginning.
    ...

--- Engine/Source/Core/UI/LightingEditor.cpp
+++ Engine/Source/Core/UI/LightingEditor.cpp
@@ -2,21 +2,33 @@
  * Copyright (c) 2024 Agua Games. All rights reserved.
  * Licensed under the Agua Games License 1.0
  */
-#pragma once
-#include <imgui.h>  // Include ImGui's header
-#include <string>
-
+#include <imgui.h>
 #include "LightingEditor.h"
 #include "hdImgui.h"
+#include "IconsMaterialSymbols.h"
 
 namespace hdImgui {
+
 void ShowLightingEditor(bool* p_open, HdEditorWindowData* windowData) 
 {
+    if (!p_open) return;
+    
     ImGui::SetNextWindowBgAlpha(windowData->globalWindowBgAlpha);
-    if (ImGui::Begin("Lighting Editor", p_open, ImGuiWindowFlags_MenuBar))
+    if (ImGui::Begin("Lighting Editor", p_open))
     {
-        ImGui::Button("Import Light Rig");
+        // Left panel (fixed width)
+        ImGui::BeginChild("LeftPanel", ImVec2(320.0f, 0), true);
+        ImGui::Text("Lights List");
+        ImGui::EndChild();
+
+        ImGui::SameLine();
+
+        // Right panel (fills remaining space)
+        ImGui::BeginChild("RightPanel", ImVec2(0, 0), true);
+        ImGui::Text("Light Properties");
+        ImGui::EndChild();
     }
     ImGui::End();
 }
+
 } // namespace hdImgui
