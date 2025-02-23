f23a489c6f8381a8cbe7e4bfd404938cb844bade
Athos Correia Sampaio <athos.sampaio@gmail.com>
2025-02-22 22:00:55 -0300

    Graph Editor look getting there
    ...

--- Engine/Source/Core/UI/NodeGraphEditor.cpp
+++ Engine/Source/Core/UI/NodeGraphEditor.cpp
@@ -183,11 +183,11 @@ static void RenderExampleNode(const char* title, ImVec2 pos, HdEditorWindowData*
     // Center window title and make it slightly brighter
     ImGui::PushStyleVar(ImGuiStyleVar_WindowTitleAlign, ImVec2(0.5f, 0.5f));
     ImGui::PushStyleColor(ImGuiCol_TitleBg, ImVec4(0.3f, 0.3f, 0.3f, 0.6f));
-    ImGui::PushStyleColor(ImGuiCol_TitleBgActive, ImVec4(0.4f, 0.4f, 0.4f, 0.6f));
+    ImGui::PushStyleColor(ImGuiCol_TitleBgActive, ImVec4(0.4f, 0.4f, 0.4f, 0.8f));
     
     // Make widgets shorter in height
-    ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(4, 2));
-    ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(4, 2));
+    ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(4, 1.4f));
+    ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(4, 1.4f));
     
     if (ImGui::Begin(title, nullptr, nodeFlags)) {
         // Get window dimensions once
